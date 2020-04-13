acea0dd4c865fba9d1adf2e1f9d3f517603f0a51
x <elu.developer@gmail.com>
2020-04-13 03:37:24 +0200

    add styled main page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("\\o/")
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+
+app.listen(3000, () => console.log('app now listening for requests on port 3000'))
