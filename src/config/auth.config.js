43459708d5c4d46fc2899a40d0da6cdd4327a8a8
x <elu.developer@gmail.com>
2020-04-17 04:38:56 +0200

    fix google dependencies

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -1,5 +1,5 @@
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth2')
+const GoogleStrategy = require('passport-google-oauth20')
 
 passport.serializeUser((user, done) => {
     done(null, user.id)
