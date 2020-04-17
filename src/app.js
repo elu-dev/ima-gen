793148e91e51b71fbc53243f2f3e670708ad34a3
x <elu.developer@gmail.com>
2020-04-17 02:22:22 +0200

    reorganize files and redesign frontend

--- src/app.js
+++ src/app.js
@@ -4,12 +4,17 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.static('src/static'))
+app.use(express.static('src/views/static'))
 
-app.use('/auth', require('./routes/auth.js'))
+app.use('/auth', require('./auth.js'))
+
 
 app.get('/', (req,res) => {
     res.render('home')
+})
+
+app.get('/profile', (req,res) => {
+    res.render('profile')
 })
 
 app.listen(3000, () => console.log('app http://localhost:3000'))
