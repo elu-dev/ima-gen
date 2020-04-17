22dd1696897c1685223bfc02980f6f7a6cec274b
x <elu.developer@gmail.com>
2020-04-18 01:39:37 +0200

    fix some async issues

--- src/api/imagen.js
+++ src/api/imagen.js
@@ -5,11 +5,10 @@ const fs = require('fs')
 const SIZE = 10
 const MIDD = SIZE / 2
 
-router.get('/:id', (req,res) => {
-    res.set({'Content-Type': 'image/png'})
-
-    const ID = req.params.id
-
+/**
+ * Creates a 10x10 pattern using the bits of ID as white pixels
+ */
+async function genImage(ID) {
     let image = new Jimp(SIZE, SIZE, '#393e46', (err, image) => { if (err) throw err })
     
     for (let y = 0; y < SIZE; ++y)
@@ -17,15 +16,21 @@ router.get('/:id', (req,res) => {
         const i = y * MIDD + (x < MIDD ? x : SIZE - x - 1)
         if (((ID >>> i) & 1)) image.setPixelColor(0xFFFFFFFF, x, y)
     }
-    
-    const path = `${__dirname}/cache/${ID}.png`
 
-    // TODO: move to async/await if possible
-    image.write(
-        path, () => res.sendFile(
-            path, () => fs.unlinkSync(path)
-        )
-    )
+    return image
+}
+
+router.get('/:id', async (req,res) => {
+    res.set({'Content-Type': 'image/png'})
+
+    const image = await genImage(req.params.id)
+    
+    const path = `${__dirname}/cache/${req.params.id}.png`
+    
+    image.write(path, () => res.sendFile(path, () => fs.unlinkSync(path)))
+    // await image.write(path)
+    // await res.sendFile(path)
+    // await fs.unlinkSync(path)
 })
 
 module.exports = router
